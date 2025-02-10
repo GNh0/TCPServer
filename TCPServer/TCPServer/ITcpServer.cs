c2b060983174cdb18f7d6d524362f611ab0f2e28
GNho <jh1422@gmail.com>
2025-02-10 22:36:08 +0900

    .NET Framework 8.0 TCPServer 구현

---
+++ TCPServer/TCPServer/ITcpServer.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Net.Sockets;
+
+namespace TCP.Server
+{
+    public interface ITcpServer
+    {
+        object Port { get; set; }
+        List<byte[]> CustomEndOfMessageBytes
+        {
+            get; set;
+        }
+
+
+
+        Task StartAsync();
+        void Stop();
+        bool DisconnectClient(string ipAddress);
+        bool IsListen();
+        IEnumerable<TcpClient> GetConnectedClients();
+        bool BroadcastData(string data);
+        bool SendDataToClient(string ip, string data);
+        bool IsPortAvailable(int _nPort);
+
+        event Action<byte[], string> DataReceived;
+
+    }
+
+
+    public class DataBuffer
+    {
+        public byte[] bydata;
+
+        public string ipaddress;
+    }
+}
---
+++ TCPServer/TCPServerUnitTest/UnitTest1.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics;
+using System.Text;
+using TCP.Server;
+
+namespace TCPServerUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        ITcpServer tcpServer;
+
+        [TestMethod]
+        public void TCPServerUnitTest()
+        {
+            tcpServer = new TcpServer(1470);
+            tcpServer.DataReceived += TCP_OnDataReiceved;
+            tcpServer.
+            tcpServer.StartAsync();
+
+     
+
+        }
+
+
+        public void TCP_OnDataReiceved(byte[] data, string ip)
+        {
+            string str = Encoding.Default.GetString(data);
+
+
+            Debug.WriteLine(str);
+            Debug.WriteLine(str);
+        }
+
+    }
+}
